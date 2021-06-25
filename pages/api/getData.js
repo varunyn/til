f004efc9cb373528855a00c9b159aef9dca3eb76
varun <varun.y.n@gmail.com>
2021-06-25 18:37:28 -0500

    Fixed build error

--- pages/api/getData.js
+++ pages/api/getData.js
@@ -5,9 +5,9 @@
 //   res.status(200).json(data);
 // };
 
-import { getAllTags } from '@/lib/tag';
+// import { getAllTags } from '@/lib/tag';
 
-export default async (_, res) => {
-  const data = await getAllTags('blog');
-  res.status(200).json(data);
-};
+// export default async (_, res) => {
+//   const data = await getAllTags('blog');
+//   res.status(200).json(data);
+// };
